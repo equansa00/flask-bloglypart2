5ca0c0f63d42f81943e51d09424d75b28f50b310
equansa00 <Your GitHub Email>
2023-09-04 23:03:51 -0400

    flask-bloglypart2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from models import db, connect_db, User
 from flask_migrate import Migrate
+from models import Post
+from forms import PostForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1Chriss1@localhost/bloglypart1'
@@ -65,3 +67,38 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
     return redirect("/users")
+@app.route('/users/<int:user_id>/posts/new', methods=['GET', 'POST'])
+def new_post(user_id):
+    form = PostForm()
+    user = User.query.get_or_404(user_id)
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, user=user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(f'/users/{user_id}')
+    return render_template('new_post.html', form=form, user=user)
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_details.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        return redirect(f'/posts/{post_id}')
+    return render_template('edit_post.html', form=form, post=post)
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/users/{}'.format(post.user_id))
+
+
--- models.py
+++ models.py
@@ -15,8 +15,24 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.String, nullable=True, default=None)
+    posts = db.relationship('Post', backref='user', cascade='all, delete-orphan')
+
 
     @property
     def full_name(self):
         """Returns the full name of the user."""
         return f"{self.first_name} {self.last_name}"
+
+from datetime import datetime
+from sqlalchemy import DateTime
+
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
